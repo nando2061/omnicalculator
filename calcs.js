f3846d785660b8d3d0f081bd80af710242edd19e
nando2061 <nandom2007@hotmail.com>
2023-11-22 10:33:05 +0100

    eeee

--- calcs.js
+++ calcs.js
@@ -20,9 +20,6 @@ function kineticEnergy() {
     if (selectedOption.value == "lbs") {
         mass = mass * 0.45359237
     }
-    if (selectedOption.value == "Mg") {
-        mass = mass * 1000
-    }
     if (selectedOption.value == "g") {
         mass = mass / 1000
     }   
@@ -42,16 +39,40 @@ function kineticEnergy() {
 function gravityForce() {
     const G = 0.000000000066743
 
-    var selectMass1 = document.getElementById("massopt1")
+    var selectMass1 = document.getElementById("massopt1");
     var selectedMass1 = selectMass1.options[selectMass1.selectedIndex];
+    var selectMass2 = document.getElementById("massopt2");
+    var selectedMass2 = selectMass2.options[selectMass2.selectedIndex];
 
     let mass1 = document.getElementById("mass1").value;
     let mass2 = document.getElementById("mass2").value;
     let distance = document.getElementById("distance").value;
     let ant = document.getElementById("ant")
 
-    if (selectedMass1 == "g") {
+    if (selectedMass1.value == "lbs") {
+        mass = mass * 0.45359237
+    }
+    if (selectedMass1.value == "g") {
         mass1 = mass1 / 1000
+    }
+    if (selectedMass1.value == "mg") {
+        mass1 = mass1 / 1000000
+    }
+    if (selectedMass1.value == "µg") {
+        mass1 = mass1 / 1000000000
+    }
+
+    if (selectedMass2.value == "lbs") {
+        mass2 = mass2 * 0.45359237
+    }
+    if (selectedMass2.value == "g") {
+        mass2 = mass2 / 1000
+    }
+    if (selectedMass2.value == "mg") {
+        mass2 = mass2 / 1000000
+    }
+    if (selectedMass2.value == "µg") {
+        mass2 = mass2 / 1000000000
     }
 
     let antwoord = (G * mass1 * mass2) / (distance ** 2)
